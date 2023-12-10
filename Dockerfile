fa14144b0f3e5f53fd557ed7ece2d67eab9403a3
piyush <kesarwani.pk010@gmail.com>
2023-12-11 02:42:13 +0530

    Made some changes in the file.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
-FROM python:3.9-alpine
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+FROM python:3.12.0
 WORKDIR /app
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
-if __name__ == "__main__":
+
+@app.route('/')
+def home():
+    return render_template('index.html', message='Hello!')
+
+if __name__ == '__main__':
     app.run(debug=True)
